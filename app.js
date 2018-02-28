e00c3275755b02072f42eb4ef1bf3a639dba0a88
nicolegager <ngager23@gmail.com>
2018-02-27 22:35:45 -0500

    Add Google strategy

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 const app = express();
+const auth = require('./routes/auth');
 const port = process.env.PORT || 5000;
+
+require('./config/passport')(passport);
 
 app.get('/', (request, response) => {
   response.send('It Works!');
 });
+
+app.use('/auth', auth);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI: 'mongodb://ngager23:Coolio123.@ds127993.mlab.com:27993/storybooks-dev',
   googleClientID: '694683241204-pbvul5upteabkoloffjesr539ad3cudv.apps.googleusercontent.com',
-  googleClientSecret:   tIg9v_WMQXIYEckAFz6awTy6
+  googleClientSecret: 'tIg9v_WMQXIYEckAFz6awTy6'
 }
