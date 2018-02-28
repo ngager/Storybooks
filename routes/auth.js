26fc692e531a9977b8b36640bd611a7d032433dc
nicolegager <ngager23@gmail.com>
2018-02-28 15:00:27 -0500

    Handling authentication

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,11 @@ const express =  require('express');
 const router = express.Router();
 const passport = require('passport');
 
-router.get('/google', passport.authenticate('google', {scope: ['profile', 'email']}));
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+router.get('/google/callback', passport.authenticate('google', { 
+  failureRedirect: '/' }), (request, response) => {
+    response.redirect('/dashboard');
+  });
 
 module.exports = router;
