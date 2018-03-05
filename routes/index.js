287b855ba4bb9272add15dcded052d3b51dd5cdb
nicolegager <ngager23@gmail.com>
2018-03-05 17:27:21 -0500

    Add about route and UI

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,8 @@ router.get('/dashboard', (request, response) => {
   response.render('index/dashboard');
 });
 
+router.get('/about', (request, response) => {
+  response.render('index/about');
+});
+
 module.exports = router;
