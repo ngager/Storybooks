8eba02ea20270506c6abaea523b6035c218ed940
nicolegager <ngager23@gmail.com>
2018-03-06 13:03:24 -0500

    Ensure the user is authenticated before allowing them to access the add stories endpoint

--- routes/stories.js
+++ routes/stories.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const {ensureAuthenticated} = require('../helpers/auth');
 
 router.get('/', (request, response) => {
   response.render('stories/index');
 });
 
-router.get('/add', (request, response) => {
+router.get('/add', ensureAuthenticated, (request, response) => {
   response.render('stories/add');
 });
 
